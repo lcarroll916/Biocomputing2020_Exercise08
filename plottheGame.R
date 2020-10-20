ac579de5af9e0740d934a8c5e872746f84613d59
Laura Carroll <lcarroll916@gmail.com>
2020-10-20 14:42:19 -0400

    Finished exercise for plotting the b-ball game points

--- plottheGame.R
+++ plottheGame.R
@@ -6,6 +6,7 @@
 ### 1 - Create a graph that shows how the points between two teams changes throughout a game
 # Step 1: Read in the "UWvsMSU_1-22-13.txt" as a data frame
 setwd("~/Biocomputing/R_programming/Biocomputing2020_Exercise08/")
+# Need to read as delim since it is tab delimited
 gameScores <- read.delim("UWvMSU_1-22-13.txt", header = TRUE)
 
 # Step 2: Break the table apart into two data frames that contain when each team
@@ -21,16 +22,25 @@ MSUVec <- numeric(length = nrow(MSUScore))
 MSUScore$OverallScore <- MSUVec
 
 # Step 3: Fill in the running score
-# This goes through and sums up the score for each time the teams scores
+# This goes through and sums up the running score for each time the teams score
 for (i in 1:nrow(UWScore)){
   UWScore$OverallScore[i] = sum(UWScore$score[1:i])
 }
 for (i in 1:nrow(MSUScore)){
   MSUScore$OverallScore[i] = sum(MSUScore$score[1:i])
 }
-# Step 2: Need to figure out how plotting works
 
-# Step 3: Need to make sure it's plotting the two lines separate
-
-# Step 4: Make the graph have a key so that people know which colored line matches
-#which team?
+# Step 4: Make the vectors that will be used for the plotting
+# timepoints and pointchange correspond with UW data, but I changed their names
+#so that the x and y axes would be labeled with a good name
+timepoints <- c(0, UWScore$time)
+pointchange <- c(0, UWScore$OverallScore)
+MSUtimes <- c(0, MSUScore$time)
+MSUoverall <- c(0, MSUScore$OverallScore)
+# Step 4: Plot these two lines using subsections time and OverallScore for x and y
+#respectively
+plot(timepoints, pointchange, type = 'l', col = "dark red")
+# This next step plots the MSU score on the same graph as UW using the lines
+#function in R
+# The col input tells R what color to make the line
+lines(MSUtimes, MSUoverall, col = "green")
